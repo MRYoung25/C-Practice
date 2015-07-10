c4489908b9ba56caeeca1859b36e5367b4ab8d86
MRYoung <mryoungforever33@gmail.com>
2015-07-10 10:31:39 +0800

    Create class.cpp

---
+++ class.cpp
@@ -0,0 +1,12 @@
+#include "class.h"
+#include <iostream>
+
+void Person::Print()
+{
+    std::cout << "Hello" << std::endl;
+};
+
+void Person::Setname()
+{
+    std::cout << "A New Name: " << std::endl;
+};
