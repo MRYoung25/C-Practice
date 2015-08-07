0966aa827b6aee3ddcf024efc92974aa2603dc25
MRYoung <mryoungforever33@gmail.com>
2015-08-07 16:05:23 +0800

    Create Transform yards.cpp

---
+++ Transform yards.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Please iuput the distance: ";
+	int Long;
+	cin >> Long;
+	int Yards;
+	Yards = Long * 220;
+	cout << "The transform of result is: " << Yards << " yards" << endl;
+	return 0;
+};
